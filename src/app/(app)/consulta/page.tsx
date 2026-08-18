6df3ec26591c30accbdd990afcd819826015dd15
NUBIA DE ANDRADE <92471268+nubia-andrade@users.noreply.github.com>
2026-08-17 23:02:55 -0300

    feat: conecta seleção de cliente e programa na nova consulta

--- src/app/(app)/consulta/page.tsx
+++ src/app/(app)/consulta/page.tsx
@@ -1,5 +1,15 @@
-import { SecaoEmConstrucao } from '@/components/layout/SecaoEmConstrucao'
+import { InicioDaConsulta } from '@/components/consulta/InicioDaConsulta'
+import { listarProgramas } from '@/lib/dados/programas'
 
-export default function PaginaConsulta() {
-  return <SecaoEmConstrucao titulo="Nova consulta" />
+export default async function PaginaConsulta() {
+  const programas = (await listarProgramas())
+    .filter((programa) => programa.estado === 'ativo' && programa.disponivel_para_proposta)
+    .map((programa) => ({
+      id: programa.id,
+      nome: programa.nome,
+      canal: programa.canal,
+      aceita_regional: programa.aceita_regional,
+    }))
+
+  return <InicioDaConsulta programas={programas} />
 }
